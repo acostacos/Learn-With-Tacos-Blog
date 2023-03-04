8351b0e493ef9d8df82c2649ac36dec709ad600f
Carlo Acosta <acostacarlom@gmail.com>
2023-03-04 11:09:53 +0800

    Added additional components and eslint to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
+import Learn from './pages/Learn/Learn';
+
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Learn />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Learn/Learn.js
@@ -0,0 +1,14 @@
+import Nav from '../../components/Nav';
+
+import React from 'react';
+
+const Learn = () => {
+  return (
+    <div>
+        <Nav />
+        Learn
+    </div>
+  );
+};
+
+export default Learn;
